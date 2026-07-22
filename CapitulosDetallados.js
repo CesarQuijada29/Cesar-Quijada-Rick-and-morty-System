46b76b5be9ddc0e4043223a06a8335267072e8c0
Cesar Quijada <caquijada.25@est.ucab.edu.ve>
2026-07-21 21:23:05 -0400

    System V. 1.4

---
+++ CapitulosDetallados.js
@@ -0,0 +1,74 @@
+let datosRecuperados = localStorage.getItem('cuentaActiva');
+let cuentaActual = datosRecuperados ? JSON.parse(datosRecuperados) : null;
+let datosanteriores = localStorage.getItem('usuariosRegistrados');
+let listaUsuarios = datosanteriores ? JSON.parse(datosanteriores) : [];
+let datosCapitulo = localStorage.getItem('CapituloActivo');
+let CapituloActual = datosCapitulo ? JSON.parse(datosCapitulo) : null;
+let datosCapitulos = localStorage.getItem('datosCapitulos');
+let listaCapitulos = datosCapitulos ? JSON.parse(datosCapitulos) : [
+    {ID:1,name: "Pilot", fecha: "2 de diciembre de 2013", codigo: "EP#1"},
+    {ID:2,name: "Lawnmower Dog", fecha: "9 de diciembre de 2013", codigo: "EP#2"},
+    {ID:3,name: "Anatomy Park", fecha: "16 de diciembre de 2013", codigo: "EP#3"},
+    {ID:4,name: "M. Night Shaym-Aliens!", fecha: "13 de enero de 2014", codigo: "EP#4"},
+    {ID:5,name: "Meeseeks and Destroy", fecha: "20 de enero de 2014", codigo: "EP#5"},
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    //nombre de usuario 
+    const Username = document.getElementById("ProfilePicture");
+    const actualUserName=cuentaActual.nombre
+    let Mode=cuentaActual.mode;
+    if (Mode===1){
+        document.body.classList.toggle("dark-mode");
+    }
+    Username.textContent = actualUserName;
+    // Cerrar Sesión
+    const btnCerrar = document.getElementById("SalirButton");
+    if (btnCerrar) {
+        btnCerrar.addEventListener("click", () => {
+            window.location.href = "LogIn.html";
+        });
+    }
+    const btnAtras = document.getElementById("AtrasButton");
+    if (btnAtras) {
+        btnAtras.addEventListener("click", () => {
+            window.location.href = "Episodios.html";
+        });
+    }
+    const InfoNombre = document.getElementById("Nombre");
+    InfoNombre.textContent=CapituloActual.name;
+    InfoNombre.id = 'Titulo';
+    InfoNombre.style.fontSize = "5em";
+    const InfoID = document.getElementById("ID");
+    InfoID.textContent="ID: " + CapituloActual.ID;
+    InfoID.style.fontSize = "2em"
+    const InfoFecha = document.getElementById("Fecha");
+    InfoFecha.textContent="Fecha de emisión: "+ CapituloActual.fecha;
+    InfoFecha.style.fontSize = "2em"
+    const InfoCodigo = document.getElementById("Codigo");
+    InfoCodigo.textContent="Código: "+ CapituloActual.codigo;
+    InfoCodigo.style.fontSize = "2em"
+
+    //seccion de edición
+    const inputID = document.getElementById("NewID");
+    const inputFecha = document.getElementById("NewFecha");
+    const inputCodigo = document.getElementById("NewCodigo");
+
+    document.querySelector('#btn-Editar').addEventListener('click', (e) => { 
+        e.preventDefault();       
+        if(inputID.value && inputFecha.value && inputCodigo.value){
+            CapituloActual.ID=inputID.value;
+            CapituloActual.fecha=inputFecha.value;
+            CapituloActual.codigo=inputCodigo.value;
+            const idx = listaCapitulos.findIndex(u => u.name === CapituloActual.name);
+            if (idx !== -1) listaCapitulos[idx] = CapituloActual;
+            localStorage.setItem('datosCapitulos', JSON.stringify(listaCapitulos));
+            localStorage.setItem('CapituloActivo', JSON.stringify(CapituloActual));
+            alert(`¡Edición exitosa ${actualUserName}!\n`);
+            window.location.href = "CapitulosDetallados.html";
+        }
+        else{
+            alert(`¡Edición Fallida!\n\nRevisa si llenaste bien los datos :[, ${actualUserName}.\n`);
+        }
+    });
+});
--- EpisodiosLogic.js
+++ EpisodiosLogic.js
@@ -3,6 +3,25 @@ let datosRecuperados = localStorage.getItem('cuentaActiva');
 let cuentaActual = datosRecuperados ? JSON.parse(datosRecuperados) : null;
 let datosanteriores = localStorage.getItem('usuariosRegistrados');
 let listaUsuarios = datosanteriores ? JSON.parse(datosanteriores) : [];
+//Personajes
+class Capitulo {
+    constructor(ID,name, fecha, codigo) {
+        this.ID=ID
+        this.name = name;
+        this.fecha=fecha;
+        this.codigo=codigo;
+    }
+}
+let datosCapitulos = localStorage.getItem('datosCapitulos');
+let listaCapitulos = datosCapitulos ? JSON.parse(datosCapitulos) : [
+    {ID:1,name: "Pilot", fecha: "2 de diciembre de 2013", codigo: "EP#1"},
+    {ID:2,name: "Lawnmower Dog", fecha: "9 de diciembre de 2013", codigo: "EP#2"},
+    {ID:3,name: "Anatomy Park", fecha: "16 de diciembre de 2013", codigo: "EP#3"},
+    {ID:4,name: "M. Night Shaym-Aliens!", fecha: "13 de enero de 2014", codigo: "EP#4"},
+    {ID:5,name: "Meeseeks and Destroy", fecha: "20 de enero de 2014", codigo: "EP#5"},
+];
+localStorage.setItem('datosCapitulos', JSON.stringify(listaCapitulos));
+const Tabla= document.getElementById('ContentTable');
 
 darkMode.addEventListener("click", (e) => {
     e.preventDefault();
@@ -35,13 +54,72 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = "LogIn.html";
         });
     }
+    const btnbuscar = document.getElementById("btn-buscar");
+    if (btnbuscar) {
+        btnbuscar.addEventListener("click", () => {
+            let CapituloIngresado = document.querySelector('input[type="text"]').value;
+            let encontrado=0;
+            if(CapituloIngresado){
+                for (const Capitulo of listaCapitulos) {
+                    console.log(Capitulo);
+                    let comparacion= Capitulo.name;
+                    if (comparacion===CapituloIngresado){
+                        encontrado=1;
+                        localStorage.setItem('CapituloActivo', JSON.stringify(Capitulo));
+                    }
+                }
+                if (encontrado===1){
+                    window.location.href="CapitulosDetallados.html";
+                    alert("Capitulo Encontrado")
+                }
+                else{
+                    alert("No encontrado")
+                }
+            }
+            else{
+                alert("Tienes que llenar la barra de búsqueda");
+            }
+        });
+    }
     const Personajes = document.getElementById("PersonajesButton");
     if (Personajes) {
         Personajes.addEventListener("click", () => {
             window.location.href = "Personajes.html";
         });
     }
+    //colocacion de los personajes
+    cargarDatosDeCapitulo();
+    //Preparacion de los botones
+    document.querySelectorAll(".cajaCapitulo").forEach(elemento => {
+    elemento.addEventListener('click', (e) => { 
+        e.preventDefault();
+        let textoElemento = e.currentTarget.textContent.trim();
+
+        let CapituloEncontrado = listaCapitulos.find(episode =>
+            episode.name === textoElemento
+        );
+        console.log(CapituloEncontrado);
+        if (CapituloEncontrado) {
+            localStorage.setItem('CapituloActivo', JSON.stringify(CapituloEncontrado));
+            console.log(CapituloEncontrado);
+            window.location.href="CapitulosDetallados.html";
+        } else {
+            alert("ERROR: FALLO AL ENCONTRAR INFORMACION DEL CAPITULO");
+        }
+    });
 });
+});
+
+function cargarDatosDeCapitulo(){
+    for (const Capitulo of listaCapitulos) {
+        console.log(Capitulo);
+        const boton = document.createElement('div');
+        NewText= Capitulo.name;
+        boton.textContent= NewText;
+        boton.classList.add("cajaCapitulo");
+        Tabla.appendChild(boton);
+    }
+}
 
 function guardarCambiosGlobales() {
     const idx = listaUsuarios.findIndex(u => u.nombre === cuentaActual.nombre);
